c8439cb04d10271bf93719116eb2aba0454790db
Mike Barkmin <mike.barkmin@uni-due.de>
2021-11-12 15:20:49 +0100

    make tbe and tbr context aware

--- plugins/toolbox/toolbox.plugin.zsh
+++ plugins/toolbox/toolbox.plugin.zsh
@@ -20,6 +20,22 @@ function toolbox_prompt_info() {
   fi
 }
 
+function toolbox_name {
+  # Get absolute path, resolving symlinks
+  local PROJECT_ROOT="${PWD:A}"
+  while [[ "$PROJECT_ROOT" != "/" && ! -e "$PROJECT_ROOT/.toolbox" &&
+    ! -d "$PROJECT_ROOT/.git" ]]; do
+    PROJECT_ROOT="${PROJECT_ROOT:h}"
+  done
+
+  # Check for toolbox name override
+  if [[ -f "$PROJECT_ROOT/.toolbox" ]]; then
+    echo "$(cat "$PROJECT_ROOT/.toolbox")"
+  elif [[ -d "$PROJECT_ROOT/.git" && -n $TOOLBOX_DEFAULT_CONTAINER ]]; then
+    echo "$TOOLBOX_DEFAULT_CONTAINER"
+  fi
+}
+
 # Automatically enter Git projects in the default toolbox container. Toolbox
 # container can be overridden by placing a .toolbox file in the project root
 # with a container name in it.
@@ -27,22 +43,7 @@ function toolbox_prompt_info() {
 function toolbox_cwd {
   if [[ -z "$TOOLBOX_CWD" ]]; then
     local TOOLBOX_CWD=1
-    # Get absolute path, resolving symlinks
-    local PROJECT_ROOT="${PWD:A}"
-    while [[ "$PROJECT_ROOT" != "/" && ! -e "$PROJECT_ROOT/.toolbox" &&
-      ! -d "$PROJECT_ROOT/.git" ]]; do
-      PROJECT_ROOT="${PROJECT_ROOT:h}"
-    done
-
-    if 
-    # Check for toolbox name override
-    if [[ -f "$PROJECT_ROOT/.toolbox" ]]; then
-      TOOLBOX_NAME="$(cat "$PROJECT_ROOT/.toolbox")"
-    elif [[ -d "$PROJECT_ROOT/.git" && -n $TOOLBOX_DEFAULT_CONTAINER ]]; then
-      TOOLBOX_NAME="$TOOLBOX_DEFAULT_CONTAINER"
-    else
-      unset TOOLBOX_NAME
-    fi
+    local TOOLBOX_NAME=$(toolbox_name)
 
     if [[ -n $TOOLBOX_NAME ]]; then
       if ! is_in_toolbox; then
@@ -66,7 +67,6 @@ if [[ ! $DISABLE_TOOLBOX_ENTER -eq 1 ]]; then
   # http://zsh.sourceforge.net/Doc/Release/Functions.html
   autoload -Uz add-zsh-hook
   add-zsh-hook chpwd toolbox_cwd
-  add-zsh-hook precmd toolbox_cwd
 fi
 
 if [[ -n "$TOOLBOX_DEFAULT_IMAGE" ]]; then
@@ -76,8 +76,38 @@ else
 fi
 alias tbi="echo $TOOLBOX_DEFAULT_CONTAINER > .toolbox && toolbox_cwd"
 alias tb="toolbox"
-alias tbe="toolbox enter"
 alias tbrm="toolbox rm"
 alias tbrmi="toolbox rmi"
 alias tbl="toolbox list"
-alias tbr="toolbox run"
+
+function tbe {
+  local TOOLBOX_NAME=$(toolbox_name)
+
+  if [[ -n "$1" ]]; then
+    TOOLBOX_NAME=$1
+  elif [[ -z $TOOLBOX_NAME ]]; then
+    TOOLBOX_NAME=$TOOLBOX_DEFAULT_CONTAINER
+  fi
+
+  if ! $(podman container exists $TOOLBOX_NAME); then
+    tbc $TOOLBOX_NAME
+  fi
+
+  toolbox --container $TOOLBOX_NAME run sudo hostname $TOOLBOX_NAME
+  toolbox enter $TOOLBOX_NAME
+}
+
+function tbr {
+  local TOOLBOX_NAME=$(toolbox_name)
+
+  if $(podman container exists $1); then
+    TOOLBOX_NAME=$1
+    shift
+  elif [[ -z $TOOLBOX_NAME ]]; then
+    TOOLBOX_NAME=$TOOLBOX_DEFAULT_CONTAINER
+  fi
+
+  toolbox --container $TOOLBOX_NAME run sudo hostname $TOOLBOX_NAME
+  toolbox --container $TOOLBOX_NAME run "$@"
+}
+
